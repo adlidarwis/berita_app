38a119f91bd53f769df97cf113c870213bb4d0bc
Muhamad Adli <103436849+adlidarwis@users.noreply.github.com>
2023-05-12 05:10:46 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/projekberita/Berita9.java
@@ -0,0 +1,30 @@
+package com.example.projekberita;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Berita9 extends AppCompatActivity {
+
+
+private ImageView button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita_entertainmen2);
+
+        button = findViewById(R.id.beritaback1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Berita9.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
